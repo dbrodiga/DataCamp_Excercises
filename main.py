08ab4b489eafc864f4591fadf4b989f9047ed340
DBrodiga <david.brodigan@aig.com>
2021-06-21 22:19:38 +0100

    Module 4 DataCamp Ch 2

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # Import the data
 import pandas as pd
 import matplotlib.pyplot as plt
-# Peak at top five rows
-avocados = pd.read_pickle('avoplotto.pkl')
-avocados[['date']] = avocados[['date']].astype('datetime64')
-avocados['year'] = avocados['date'].dt.year
-print(avocados.info())
-avocados_2016 = avocados[avocados['date'].dt.year == '2016']
-print(avocados_2016.info())
 
+taglines = pd.read_pickle('DataCamp M4/taglines.p')
+movies = pd.read_pickle('DataCamp M4/movies.p')
 
-
+movies_taglines = movies.merge(taglines, on='id', how='left')
+print(movies_taglines.head())
