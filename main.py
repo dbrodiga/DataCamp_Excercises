6bb2089356245d390424d57dc783381de1f29c8c
DBrodiga <david.brodigan@aig.com>
2021-07-12 11:05:47 +0100

    Module 5 DataCamp Ch3.1

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ seattle_weather = pd.read_csv('DataCamp M5/seattle_weather.csv')
 #seattle_weather['MONTH'] = pd.to_datetime(seattle_weather['DATE'], format='%b')
 seattle_weather['MONTH'] = seattle_weather['DATE'].calendar.month_abbr
 
-Print("Hello")
+Print("Hello2")
